f2a2093c471a24a39c87ce1e390f02bf66d6b132
Mohammed Naser <mnaser@vexxhost.com>
2025-09-02 17:53:53 -0400

    chore: switch to py311 (#55)
    * chore: switch to py311
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>
    
    * chore: drop AnsibleEnvironment
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>
    
    ---------
    
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

--- plugins/action/binary_downloads.py
+++ plugins/action/binary_downloads.py
@@ -1,16 +1,6 @@
 # Copyright (c) 2024 BBC R&D, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+# Copyright (c) 2025 VEXXHOST, Inc.
+# SPDX-License-Identifier: Apache-2.0
 
 # Make coding more python3-ish, this is required for contributions to Ansible
 from __future__ import absolute_import, division, print_function
@@ -18,12 +8,23 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 from ansible.plugins.action import ActionBase
-from ansible.template import AnsibleEnvironment
+
+try:
+    from ansible.template import trust_as_template as _trust_as_template
+
+    HAS_DATATAGGING = True
+except ImportError:
+    HAS_DATATAGGING = False
+
+
+def _make_safe(value):
+    if HAS_DATATAGGING and isinstance(value, str):
+        return _trust_as_template(value)
+    return value
 
 
 class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
-
         if task_vars is None:
             task_vars = dict()
         result = super(ActionModule, self).run(tmp, task_vars)
@@ -32,7 +33,7 @@ class ActionModule(ActionBase):
         temp_vars = task_vars.copy()
         downloads = list()
 
-        templar = self._templar.copy_with_new_env(environment_class=AnsibleEnvironment)
+        templar = self._templar.copy_with_new_env()
         templar.available_variables = temp_vars
 
         for p in self._task.args.get("prefixes", None):
@@ -43,14 +44,16 @@ class ActionModule(ActionBase):
             # iterate over all defined versions for this download prefix
             for v, c in checksums["amd64"].items():
                 temp_vars[p + "_version"] = v
-                url = templar.do_template(task_vars.get(p + "_download_url"))
-                dest = templar.do_template(task_vars.get(p + "_download_dest"))
+                url = templar.template(task_vars.get(p + "_download_url"))
+                dest = templar.template(task_vars.get(p + "_download_dest"))
 
                 if dest.startswith("/usr/"):
                     temp_vars["_prefix"] = p
                     temp_vars["_version"] = v
-                    dest = templar.do_template(
-                        "{{ download_artifact_work_directory }}/{{ _prefix }}-{{ _version }}-{{ ansible_facts['system'] | lower }}-{{ download_artifact_goarch }}"  # noqa
+                    dest = self._templar.template(
+                        _make_safe(
+                            f"{{{{ download_artifact_work_directory }}}}/{p}-{v}-{{{{ ansible_facts['system'] | lower }}}}-{{{{ download_artifact_goarch }}}}"  # noqa
+                        )
                     )
 
                 res = dict()
